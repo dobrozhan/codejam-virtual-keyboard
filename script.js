3ce52e32c228de3e9aaf8522b08418d0a0cde962
keep_chr <dobrozhan.a@gmail.com>
2020-04-01 20:04:56 +0300

    feat: layout for en and ru

--- script.js
+++ script.js
@@ -1,40 +1,3 @@
-const inputField = document.createElement('textarea');
-document.querySelector('body').appendChild(inputField);
-inputField.classList.add('input-field');
-
-const keysWrapper = document.createElement('div');
-document.querySelector('body').appendChild(keysWrapper);
-inputField.insertAdjacentElement("afterend", keysWrapper);
-keysWrapper.classList.add('keys-wrapper');
-
-const changeLanguage = document.createElement('div');
-document.querySelector('body').appendChild(changeLanguage);
-changeLanguage.innerHTML = 'Apply Ctrl or left-keyboard buttons to switch layout language.';
-changeLanguage.classList.add('change-language');
-
-for (let i = 65; i > 0; i--) {
-    const key = document.createElement('div');
-    keysWrapper.appendChild(key);
-    key.classList.add('keys','inner-text');
-}
-
-document.addEventListener('keydown', function(event) {
-    if (event.keyCode == 20) {
-        const buttons = document.querySelectorAll('.keys');
-
-        buttons.forEach( e => {
-            e.classList.toggle('caps-lock');
-        });
-    }
-
-    if (event.keyCode == 16) {
-        const buttons = document.querySelectorAll('.keys');
-
-        buttons.forEach( e => {
-            e.classList.toggle('shift');
-        });
-    }
-});
 
 let russianBoard = {
     0: 'ё',
@@ -91,6 +54,74 @@ let russianBoard = {
     51: 'б',
     52: 'ю',
     53: '/',
+    54: '⇑',
+    55: 'Shift',
+    56: 'Ctrl',
+    57: 'Win',
+    58: 'Alt',
+    59: '',
+    60: 'Alt',
+    61: 'Ctrl',
+    62: '⇐',
+    63: '⇓',
+    64: '⇒'
+};
+
+let russianBoardShifted = {
+    0: 'ё',
+    1: '!',
+    2: '"',
+    3: '№',
+    4: ';',
+    5: '%',
+    6: ':',
+    7: '?',
+    8: '*',
+    9: '(',
+    10: ')',
+    11: '_',
+    12: '+',
+    13: 'Backspace',
+    14: 'Tab',
+    15: 'й',
+    16: 'ц',
+    17: 'у',
+    18: 'к',
+    19: 'е',
+    20: 'н',
+    21: 'г',
+    22: 'ш',
+    23: 'щ',
+    24: 'з',
+    25: 'х',
+    26: 'ъ',
+    27: '/',
+    28: 'Del',
+    29: 'Caps Lock',
+    30: 'ф',
+    31: 'ы',
+    32: 'в',
+    33: 'а',
+    34: 'п',
+    35: 'р',
+    36: 'о',
+    37: 'л',
+    38: 'д',
+    39: 'ж',
+    40: 'э',
+    41: 'Enter',
+    42: 'Shift',
+    43: '/',
+    44: 'я',
+    45: 'ч',
+    46: 'с',
+    47: 'м',
+    48: 'и',
+    49: 'т',
+    50: 'ь',
+    51: 'б',
+    52: 'ю',
+    53: '.',
     54: '⇑',
     55: 'Shift',
     56: 'Ctrl',
@@ -172,6 +203,95 @@ let englishBoard = {
     64: '⇒'
 };
 
+let englishBoardShifted = {
+    0: '~',
+    1: '!',
+    2: '@',
+    3: '#',
+    4: '$',
+    5: '%',
+    6: '^',
+    7: '&',
+    8: '*',
+    9: '(',
+    10: ')',
+    11: '_',
+    12: '+',
+    13: 'Backspace',
+    14: 'Tab',
+    15: 'q',
+    16: 'w',
+    17: 'e',
+    18: 'r',
+    19: 't',
+    20: 'y',
+    21: 'u',
+    22: 'i',
+    23: 'o',
+    24: 'p',
+    25: '{',
+    26: '}',
+    27: '|',
+    28: 'Del',
+    29: 'Caps Lock',
+    30: 'a',
+    31: 's',
+    32: 'd',
+    33: 'f',
+    34: 'g',
+    35: 'h',
+    36: 'j',
+    37: 'k',
+    38: 'l',
+    39: ':',
+    40: '"',
+    41: 'Enter',
+    42: 'Shift',
+    43: '\\',
+    44: 'z',
+    45: 'x',
+    46: 'c',
+    47: 'v',
+    48: 'b',
+    49: 'n',
+    50: 'm',
+    51: '<',
+    52: '>',
+    53: '?',
+    54: '⇑',
+    55: 'Shift',
+    56: 'Ctrl',
+    57: 'Win',
+    58: 'Alt',
+    59: '',
+    60: 'Alt',
+    61: 'Ctrl',
+    62: '⇐',
+    63: '⇓',
+    64: '⇒'
+};
+
+
+const inputField = document.createElement('textarea');
+document.querySelector('body').appendChild(inputField);
+inputField.classList.add('input-field');
+
+const keysWrapper = document.createElement('div');
+document.querySelector('body').appendChild(keysWrapper);
+inputField.insertAdjacentElement("afterend", keysWrapper);
+keysWrapper.classList.add('keys-wrapper');
+
+const changeLanguage = document.createElement('div');
+document.querySelector('body').appendChild(changeLanguage);
+changeLanguage.innerHTML = 'Please, select keyboard layout language below. OS Windows 10.';
+changeLanguage.classList.add('change-language');
+
+for (let i = 65; i > 0; i--) {
+    const key = document.createElement('div');
+    keysWrapper.appendChild(key);
+    key.classList.add('keys','inner-text');
+}
+
 const russianButton = document.createElement('div');
 const englishButton = document.createElement('div');
 
@@ -181,30 +301,90 @@ document.querySelector('body').appendChild(englishButton);
 russianButton.classList.add('evaluation-button','inner-text');
 englishButton.classList.add('evaluation-button','inner-text');
   
-
 russianButton.innerHTML = 'Russian';
 englishButton.innerHTML = 'English';
 
-
 russianButton.addEventListener('click', () => {
-    russianButton.classList.toggle('selected');
+    russianButton.classList.add('selected');
+    englishButton.classList.remove('selected');
     const buttons = document.querySelectorAll('.keys');
 
     buttons.forEach( (e, i = 1) => {
         e.innerHTML = russianBoard[i];
     });
 
-    
+    changeLanguage.classList.add('stop-blur-animation');
 });
 
 englishButton.addEventListener('click', () => {
-    englishButton.classList.toggle('selected');
+    englishButton.classList.add('selected');
+    russianButton.classList.remove('selected');
     const buttons = document.querySelectorAll('.keys');
 
     buttons.forEach( (e, i = 1) => {
         e.innerHTML = englishBoard[i];
     });
-})
+
+    changeLanguage.classList.add('stop-blur-animation');
+});
+
+document.addEventListener('keydown', function(event) {
+    if (event.keyCode == 20) {
+        const buttons = document.querySelectorAll('.keys');
+
+        buttons.forEach( e => {
+            e.classList.toggle('caps-lock');
+        });
+    }
+
+    if (event.keyCode == 16 && russianButton.classList.contains('selected')) {
+        const buttons = document.querySelectorAll('.keys');
+
+        buttons.forEach( e => {
+            e.classList.add('shift');
+        });
+        
+        buttons.forEach( (e, i = 1) => {
+            e.innerHTML = russianBoardShifted[i];
+        });
+    } else {
+        const buttons = document.querySelectorAll('.keys');
+
+        buttons.forEach( e => {
+            e.classList.add('shift');
+        });
+        
+        buttons.forEach( (e, i = 1) => {
+            e.innerHTML = englishBoardShifted[i];
+        });
+    }
+});
+
+document.addEventListener('keyup', function(event) {
+    if (event.keyCode == 16 && russianButton.classList.contains('selected')) {
+        const buttons = document.querySelectorAll('.keys');
+
+        buttons.forEach( e => {
+            e.classList.remove('shift');
+        });
+
+        buttons.forEach( (e, i = 1) => {
+            e.innerHTML = russianBoard[i];
+        });
+    } else {
+        const buttons = document.querySelectorAll('.keys');
+
+        buttons.forEach( e => {
+            e.classList.remove('shift');
+        });
+
+        buttons.forEach( (e, i = 1) => {
+            e.innerHTML = englishBoard[i];
+        });
+    }
+});
+
+
 
 
 
